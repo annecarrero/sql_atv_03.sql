470ce65f59fb281f594b264f554a995466c0c601
annecarrero <125596560+annecarrero@users.noreply.github.com>
2024-04-02 12:51:00 -0300

    Update sql_atv_03.sql

--- sql_atv_03.sql
+++ sql_atv_03.sql
@@ -1,2 +1,8 @@
 ALTER TABLE alunos ADD COLUMN favorites VARCHAR(3);
-UPDATE alunos SET favorites = 'SIM' WHERE ID <= 10;
+UPDATE alunos SET favorites = 'SIM' WHERE ID = 2;
+UPDATE alunos SET favorites = 'SIM' WHERE ID = 7;
+UPDATE alunos SET favorites = 'SIM' WHERE ID = 11;
+UPDATE alunos SET favorites = 'SIM' WHERE ID = 17;
+UPDATE alunos SET favorites = 'SIM' WHERE ID = 19;
+UPDATE alunos SET favorites = 'SIM' WHERE ID = 24;
+UPDATE alunos SET favorites = 'SIM' WHERE ID = 27;
